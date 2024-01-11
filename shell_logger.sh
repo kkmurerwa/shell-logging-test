629d9dc450c51ae562ff5672ef17ae1be191d753
Kenneth Murerwa <xwaxes@gmail.com>
2024-01-11 15:15:54 +0300

    fix: Minor aesthetic changes

--- shell_logger.sh
+++ shell_logger.sh
@@ -1,7 +1,7 @@
 function echo_error() {
-    echo -e "\x1B[0;31;2mHello World\x1B[0m"
+    echo -e "\x1B[0;31;1m$1\x1B[0m"
 }
 
 function echo_success() {
-    echo -e "\x1B[0;32;2mHello World\x1B[0m"
+    echo -e "\x1B[0;32;2m$1\x1B[0m"
 }
