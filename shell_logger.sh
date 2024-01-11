71022b3cd82a58413147600848fd3ef552e5782c
Kenneth Murerwa <xwaxes@gmail.com>
2024-01-11 14:22:33 +0300

    feat: initial commit

---
+++ shell_logger.sh
@@ -0,0 +1,7 @@
+function echo_error() {
+    echo -e "\x1B[0;31;2mHello World\x1B[0m"
+}
+
+function echo_success() {
+    echo -e "\x1B[0;32;2mHello World\x1B[0m"
+}
---
+++ shell_logger_tester.sh
@@ -0,0 +1,5 @@
+source ./shell_logger.sh
+
+echo_error "This is an error message"
+
+echo_success "This is a success message"
